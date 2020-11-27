6415723b822f2e2df49651194496a639d872f81f
raosan <raosan@hyperjump.tech>
2020-11-27 12:31:42 +0700

    simple UI

--- src/App.js
+++ src/App.js
@@ -16,15 +16,53 @@ function App() {
               Find out is this repo legit
             </h2>
 
-            <div class="columns">
-              <div class="column is-8 is-offset-1">
+            <div className="columns">
+              <div className="column is-8 is-offset-1">
                 <input className="input is-medium" type="text" placeholder="Enter a github repo URL" />
               </div>
-              <div class="column is-2">
-                <button class="button is-primary is-medium is-fullwidth">Run Audit</button>
+              <div className="column is-2">
+                <button className="button is-info is-outlined is-medium is-fullwidth">Run Audit</button>
               </div>
             </div>
           </div>
+        </div>
+      </section>
+      <section className="hero">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title has-text-centered">
+              Result: 
+              <span class="tag is-success is-large" style={{marginLeft: 10}}>LEGIT</span>
+            </h1>
+            <table className="table is-fullwidth">
+              <thead>
+                <tr>
+                  <th>
+                    Parameter
+                  </th>
+                  <th>
+                    Value
+                  </th>
+                  <th>
+                    Score
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Star Count</td>
+                  <td>30</td>
+                  <td>Good</td>
+                </tr>
+                <tr>
+                  <td>Star Count</td>
+                  <td>30</td>
+                  <td>Good</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        
         </div>
       </section>
     </div>
