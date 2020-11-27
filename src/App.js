597cfc1ac611dcfbbf1e364f3459206016b533f6
raosan <raosan@hyperjump.tech>
2020-11-27 13:28:45 +0700

    ui result

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
-import 'bulma/css/bulma.css'
-import Header from './components/Header'
+import "bulma/css/bulma.css";
+import Header from "./components/Header";
+import params from "./data/params";
 
 function App() {
   return (
@@ -9,19 +10,50 @@ function App() {
       <section className="hero">
         <div className="hero-body">
           <div className="container has-text-centered">
-            <h1 className="title">
-              Measure
-            </h1>
-            <h2 className="subtitle">
-              Find out is this repo legit
-            </h2>
+            <h1 className="title">Measure</h1>
+            <h2 className="subtitle">Find out is this repo legit</h2>
 
             <div className="columns">
               <div className="column is-8 is-offset-1">
-                <input className="input is-medium" type="text" placeholder="Enter a github repo URL" />
+                <input
+                  className="input is-medium"
+                  type="text"
+                  placeholder="Enter a github repo URL"
+                />
               </div>
               <div className="column is-2">
-                <button className="button is-info is-outlined is-medium is-fullwidth">Run Audit</button>
+                <button className="button is-info is-outlined is-medium is-fullwidth">
+                  Run Audit
+                </button>
+              </div>
+            </div>
+
+            <div className="box" style={{ marginTop: 50 }}>
+              <div className="content">
+                <div className="columns">
+                  <div className="column">
+                    <h1 className="title has-text-centered">
+                      Score:
+                      <span
+                        className="tag is-success is-large is-rounded"
+                        style={{ marginLeft: 10 }}
+                      >
+                        90 / 100
+                      </span>
+                    </h1>
+                  </div>
+                  <div className="column">
+                    <h1 className="title has-text-centered">
+                      Result:
+                      <span
+                        className="tag is-success is-large is-rounded"
+                        style={{ marginLeft: 10 }}
+                      >
+                        LEGIT
+                      </span>
+                    </h1>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -30,39 +62,25 @@ function App() {
       <section className="hero">
         <div className="hero-body">
           <div className="container">
-            <h1 className="title has-text-centered">
-              Result: 
-              <span class="tag is-success is-large" style={{marginLeft: 10}}>LEGIT</span>
-            </h1>
             <table className="table is-fullwidth">
               <thead>
                 <tr>
-                  <th>
-                    Parameter
-                  </th>
-                  <th>
-                    Value
-                  </th>
-                  <th>
-                    Score
-                  </th>
+                  <th>Parameter</th>
+                  <th>Value</th>
+                  <th>Score</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>Star Count</td>
-                  <td>30</td>
-                  <td>Good</td>
-                </tr>
-                <tr>
-                  <td>Star Count</td>
-                  <td>30</td>
-                  <td>Good</td>
-                </tr>
+                {params.map((item) => (
+                  <tr>
+                    <td>{item.key}</td>
+                    <td>30</td>
+                    <td>Good</td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
-        
         </div>
       </section>
     </div>
---
+++ src/data/params.js
@@ -0,0 +1,13 @@
+const params = [
+  {key: 'Star'},
+  {key: 'Last Updated'},
+  {key: 'Fork'},
+  {key: 'Open Issue'},
+  {key: 'Closed Issue'},
+  {key: 'NPM Download'},
+  {key: 'README'},
+  {key: 'Get Started'},
+  {key: 'Website / Docs'},
+]
+
+export default params
