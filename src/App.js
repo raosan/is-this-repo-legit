ffc0de637f56a4fc32f374708123a8bd71a50cec
raosan <raosan@hyperjump.tech>
2020-11-27 12:11:09 +0700

    add search input

--- src/App.js
+++ src/App.js
@@ -15,6 +15,15 @@ function App() {
             <h2 className="subtitle">
               Find out is this repo legit
             </h2>
+
+            <div class="columns">
+              <div class="column is-8 is-offset-1">
+                <input className="input is-medium" type="text" placeholder="Enter a github repo URL" />
+              </div>
+              <div class="column is-2">
+                <button class="button is-primary is-medium is-fullwidth">Run Audit</button>
+              </div>
+            </div>
           </div>
         </div>
       </section>
